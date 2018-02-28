42b493b4ea2ec5b5d14828d4b95ed4e896c9fc1a
Viacheslav Volkov <viacheslav.volkov.1@gmail.com>
2018-02-28 16:25:47 +0100

    Update setup_pi_usb_stick.sh
    automate insertion of linux kernel modules required.

--- pi_stuff/pi_zero_usb_stick/setup_pi_usb_stick.sh
+++ pi_stuff/pi_zero_usb_stick/setup_pi_usb_stick.sh
@@ -36,16 +36,20 @@ touch /boot/ssh
 
 echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
 
-#TODO
-#need to /etc/modules
-#Append the line below, just after the i2c-dev line:
-#dwc2
+cat << EOF | sudo tee -a /etc/modules
+dwc2
+g_mass_storage
+EOF
+
+echo "options g_mass_storage file=/piusb.bin stall=0 removable=1" | sudo tee /etc/modprobe.d/usb_storage.conf
 
 sudo dd bs=1M if=/dev/zero of=/piusb.bin count=4096
 sudo mkdosfs /piusb.bin -F 32 -I
 sudo mkdir /mnt/usb_share
 
 echo "/piusb.bin /mnt/usb_share vfat users,umask=000 0 2" | sudo tee -a /etc/fstab
+
+# The following commands are not needed anymore (everything should work after reboot):
 sudo mount -a
 
 sudo modprobe g_mass_storage file=/piusb.bin stall=0 removable=1
